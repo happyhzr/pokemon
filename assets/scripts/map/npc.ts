d57a9b9b449aa9c3554830bc129107c6ed1b6560
happyhzr <insisthzr@163.com>
2026-03-24 21:16:04 +0800

    add animation

---
+++ assets/scripts/map/npc.ts
@@ -0,0 +1,19 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('npc')
+export class npc extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+
+    talk() {
+
+    }
+}
+
+
---
+++ assets/scripts/map/player_move.ts
@@ -0,0 +1,16 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('player_move')
+export class player_move extends Component {
+    static canControl = true
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
+
