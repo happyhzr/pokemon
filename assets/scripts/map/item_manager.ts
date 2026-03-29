1910f47ceb93f131f8c3f5b08dfe09864f2b3723
happyhzr <insisthzr@163.com>
2026-03-29 21:20:49 +0800

    add inventory

---
+++ assets/scripts/map/item_manager.ts
@@ -0,0 +1,84 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+export enum ItemType {
+    None,
+    Health,
+}
+
+export class Item {
+    itemID: number
+    itemName: string
+    type: ItemType
+    power: number
+    count: number = 1
+}
+
+
+export class ItemManager {
+    private static instance: ItemManager
+    itemMap = new Map<number, Item>()
+
+    static getInstance() {
+        if (ItemManager.instance == null) {
+            ItemManager.instance = new ItemManager()
+        }
+        return ItemManager.instance
+    }
+
+    constructor() {
+        let item = new Item()
+        item.itemID = 1
+        item.itemName = "Potion"
+        item.type = ItemType.Health
+        item.power = 20
+        item.count = 1
+        this.itemMap.set(item.itemID, item)
+    }
+}
+
+export class Inventory {
+    private static instance: Inventory
+    itemMap = new Map<number, Item>()
+
+    static getInstance() {
+        if (Inventory.instance == null) {
+            Inventory.instance = new Inventory()
+        }
+        return Inventory.instance
+    }
+
+    getItem(itemID: number) {
+        if (this.itemMap.has(itemID)) {
+            return this.itemMap.get(itemID)
+        }
+        return null
+    }
+
+    addItem(itemID: number) {
+        let item = this.getItem(itemID)
+        if (item == null) {
+            let newItem = ItemManager.getInstance().itemMap.get(itemID)
+            if (newItem) {
+                this.itemMap.set(itemID, newItem)
+            }
+        } else {
+            item.count++
+        }
+    }
+
+    removeItem(itemID: number) {
+        let item = this.getItem(itemID)
+        if (item == null) {
+            return false
+        }
+        if (item.count > 0) {
+            item.count--
+            if (item.count == 0) {
+                this.itemMap.delete(itemID)
+            }
+            return true
+        }
+    }
+}
+
