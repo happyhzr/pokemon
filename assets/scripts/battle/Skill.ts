7208523e86da7f2e3ab4e30c39368fbb7998bde6
happyhzr <insisthzr@163.com>
2026-04-05 19:41:10 +0800

    add pokemon & skill

---
+++ assets/scripts/battle/Skill.ts
@@ -0,0 +1,22 @@
+import { _decorator, CCInteger, CCString, Component, Enum, Node } from 'cc';
+import { PokemonType } from './Pokemon';
+const { ccclass, property } = _decorator;
+
+@ccclass('Skill')
+export class Skill extends Component {
+    @property({ type: CCString })
+    skillName: string = ""
+    @property({ type: CCInteger })
+    power: number = 0
+    @property({ type: Enum(PokemonType) })
+    type: PokemonType
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
+
