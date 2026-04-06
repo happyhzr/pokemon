28304738c30c9e8ae92be39ad5c90d80b073bbce
happyhzr <insisthzr@163.com>
2026-04-06 16:45:58 +0800

    add pokemons

--- assets/scripts/battle/BattleManager.ts
+++ assets/scripts/battle/BattleManager.ts
@@ -1,15 +1,59 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab, resources } from 'cc';
+import { Pokemon } from './Pokemon';
+import { Item } from '../map/item_manager';
+import { TeamManager } from './TeamManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('BattleManager')
 export class BattleManager extends Component {
-    static enemyPokemonID: number = 0
+    static enemyPokemonID: number = 2
+    static instance: BattleManager
+
+    @property({ type: Node })
+    playerParent: Node
+    @property({ type: Node })
+    enemyParent: Node
+
+    private player: Pokemon
+    private enmey: Pokemon
+    private index: number = 0
+    private isPlayer: boolean = false
+    private playerSkill: string = ""
+    private enmeySkill: string = ""
+    private enemtyItem: Item
 
     start() {
-
+        BattleManager.instance = this
+        this.scheduleOnce(this.turnStart, 1)
     }
 
     update(deltaTime: number) {
+
+    }
+
+    turnStart() {
+        if (this.index < TeamManager.getInstance().getPokemons().length) {
+            const index = TeamManager.getInstance().getPokemons()[this.index]
+            resources.load(`pokemon/${index}`, Prefab, (err, prefab) => {
+                this.index++
+                const playerNode = instantiate(prefab)
+                playerNode.setParent(this.playerParent)
+                this.player = playerNode.getComponent(Pokemon)
+            })
+        }
+        resources.load(`pokemon/${BattleManager.enemyPokemonID}`, Prefab, (err, prefab) => {
+            const enemyNode = instantiate(prefab)
+            enemyNode.setParent(this.enemyParent)
+            this.enmey = enemyNode.getComponent(Pokemon)
+        })
+        this.scheduleOnce(this.playerAction, 1)
+    }
+
+    playerAction() {
+
+    }
+
+    enemyAction() {
 
     }
 }
